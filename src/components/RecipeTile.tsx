6e9be39e77f63bf483d6bf4d1780b29df4270ff6
Dustin Popp <dpopp07@gmail.com>
2025-02-08 10:02:35 -0600

    make tiles links to mock recipe page
    Signed-off-by: Dustin Popp <dpopp07@gmail.com>

--- src/components/RecipeTile.tsx
+++ src/components/RecipeTile.tsx
@@ -1,10 +1,16 @@
+import { Link } from 'wouter';
 import classes from './RecipeTile.module.css';
 
-// !!! define type for input
-export default function RecipeTile({ name }) {
+interface Props {
+  name: string;
+}
+
+export default function RecipeTile({ name }: Props) {
   return (
-    <li className={classes.recipe}>
-      <p className={classes.title}> {name} </p>
-    </li>
+    <Link to="/recipe" className={classes.tileLink}>
+      <li className={classes.recipe}>
+        <p className={classes.title}> {name} </p>
+      </li>
+    </Link>
   );
 }
