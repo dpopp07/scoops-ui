1bfe2d21b316a9836975ccf1167e38ae5cd90502
Dustin Popp <dpopp07@gmail.com>
2025-02-22 16:42:16 -0600

    make scoops title a link to home
    Signed-off-by: Dustin Popp <dpopp07@gmail.com>

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'wouter';
 import scoopsLogo from '../assets/logo-blue.svg';
 import classes from './MainHeader.module.css';
 import NavBar from './NavBar';
@@ -6,7 +7,9 @@ export default function MainHeader() {
   return (
     <>
       <header className={classes.header}>
-        <h1 className={classes.title}>Scoops</h1>
+        <Link to="/" className={classes.titleLink}>
+          <h1 className={classes.title}>Scoops</h1>
+        </Link>
         <img className={classes.logo} src={scoopsLogo} />
         <h6 className={classes.subtitle}>Ice cream recipes and analysis</h6>
       </header>
--- src/components/RecipeTile.tsx
+++ src/components/RecipeTile.tsx
@@ -2,14 +2,13 @@ import { Link } from 'wouter';
 import classes from './RecipeTile.module.css';
 
 interface Props {
-  id: string;
   name: string;
   nameKey: string;
 }
 
-export default function RecipeTile({ id, name, nameKey }: Props) {
+export default function RecipeTile({ name, nameKey }: Props) {
   return (
-    <Link to={`/recipes/${nameKey}`} className={classes.tile} state={{ id }}>
+    <Link to={`/recipes/${nameKey}`} className={classes.tile}>
       <li>
         <p className={classes.title}> {name} </p>
       </li>
